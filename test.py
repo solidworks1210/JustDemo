d1c99f91257dfa9a9c05101c45e5e74f73aeb70a
mofei <mofeimofei1210@163.com>
2018-07-23 23:04:34 +0800

    [初始化]添加django, log

---
+++ test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import logging
+
+
+print logging.getLogger()
+print logging.getLogger('ehl')
+print logging.getLogger()
+print logging.getLogger('ehl')
